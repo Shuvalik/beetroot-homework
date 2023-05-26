818d13cdb9f4d935f7f05ee90eb223c425ef16d1
Shuvalik <shuvalova.ev@gmail.com>
2023-05-26 15:12:46 +0300

    add first js dz

--- dz-js-1/assets/js/dist/main.dev.js
+++ dz-js-1/assets/js/dist/main.dev.js
@@ -1,30 +1,87 @@
 "use strict";
 
-function revert() {
-  var number = +prompt('Enter number');
-  var result = 0; //12345 => 51234;
+//valid variable
+var name = 'Elena';
+var userName = 'Elena';
+var firsName = 'Elena'; //camelCase
 
-  if (number >= 10000 && number <= 99999) {
-    result = number % 10 * 10000 + Math.trunc(number / 10);
-    alert(result);
-  } else {
-    alert('Please enter number between 10000 and 99999');
+var current_Name = 'Elena'; //snack_case
+// invalid variable
+
+/*let n/* = 'Elena';
+let n123 = 'Elena';
+let 0terw = 'Elena';*/
+
+function hello() {
+  var user = prompt('Введить ваше ім’я');
+
+  if (user) {
+    document.getElementById('hello').innerText = "\u041F\u0440\u0438\u0432\u0456\u0442, ".concat(user, "!");
   }
 }
 
-function kl2ml() {
-  var number = +prompt('Enter km');
-  var k = 0.621371;
-  var result = 0;
+; //hello();
 
-  if (number) {
-    result = number * k;
-    alert("in ".concat(number, "km - ").concat(result, "ml"));
+function age(el) {
+  var userAge = +prompt('Введить ваше рік народження');
+  var date = new Date();
+  var currentYear = date.getFullYear();
+  var resultAges = currentYear - userAge;
+
+  if (!isNaN(userAge) && userAge && resultAges > 0 && resultAges < 120) {
+    el.previousElementSibling.innerText = "\u0412\u0430\u0448 \u0432\u0456\u043A - ".concat(resultAges);
   } else {
-    alert('Please enter number');
+    el.previousElementSibling.innerText = "\u0412\u0432\u0435\u0434\u0438\u0442\u044C \u043A\u043E\u0440\u0440\u0435\u043A\u0442\u043D\u043E \u0432\u0430\u0448 \u0440\u0456\u043A \u043D\u0430\u0440\u043E\u0434\u0436\u0435\u043D\u043D\u044F";
+  }
+
+  ;
+}
+
+;
+
+function perimetr(el) {
+  var number = +prompt('Введить довжину сторони квадратa');
+
+  if (!isNaN(number) && number && Math.sign(number) !== -1) {
+    el.previousElementSibling.innerText = "\u043F\u0435\u0440\u0438\u043C\u0435\u0442\u0440 \u043A\u0432\u0430\u0434\u0440\u0430\u0442a - ".concat(number * 4);
+  } else {
+    el.previousElementSibling.innerText = "\u0412\u0432\u0435\u0434\u0438\u0442\u044C \u043A\u043E\u0440\u0440\u0435\u043A\u0442\u043D\u043E \u0434\u043E\u0432\u0436\u0438\u043D\u0443 \u0441\u0442\u043E\u0440\u043E\u043D\u0438 \u043A\u0432\u0430\u0434\u0440\u0430\u0442a";
   }
 }
 
-if ("0") {
-  alert("Hello World");
+function ploshaKola(el) {
+  var number = +prompt('Введить радіус кола');
+
+  if (!isNaN(number) && number && Math.sign(number) !== -1) {
+    el.previousElementSibling.innerText = "\u043F\u043B\u043E\u0449a \u043E\u043A\u0440\u0443\u0436\u043D\u043E\u0441\u0442\u0456 - ".concat(Math.PI * Math.pow(number, 2));
+  } else {
+    el.previousElementSibling.innerText = "\u0412\u0432\u0435\u0434\u0438\u0442\u044C \u043A\u043E\u0440\u0440\u0435\u043A\u0442\u043D\u043E \u0440\u0430\u0434\u0456\u0443\u0441 \u043A\u043E\u043B\u0430";
+  }
+}
+
+function speedUser(el) {
+  var lengthBetweenSities = +prompt('Введить відстань в кілометрах між двома містами(тільки число)');
+
+  if (!isNaN(lengthBetweenSities) && lengthBetweenSities && Math.sign(lengthBetweenSities) !== -1) {
+    var timeDirections = +prompt('Введить за скільки годин ви хочете дістатися до іншого міста(тільки число)');
+
+    if (!isNaN(timeDirections) && timeDirections && Math.sign(timeDirections) !== -1) {
+      el.previousElementSibling.innerText = "\u0412\u0430\u043C \u043D\u0435\u043E\u0431\u0445\u0456\u0434\u043D\u043E \u0440\u0443\u0445\u0430\u0442\u0438\u0441\u044C \u0437\u0456 \u0448\u0432\u0438\u0434\u043A\u0456\u0441\u0442\u044E ".concat(lengthBetweenSities / timeDirections, " \u043A\u043C/\u0433\u043E\u0434.");
+    } else {
+      el.previousElementSibling.innerText = "\u0412\u0432\u0435\u0434\u0438\u0442\u044C \u043A\u043E\u0440\u0440\u0435\u043A\u0442\u043D\u043E \u0437\u0430 \u0441\u043A\u0456\u043B\u044C\u043A\u0438 \u0433\u043E\u0434\u0438\u043D \u0432\u0438 \u0445\u043E\u0447\u0435\u0442\u0435 \u0434\u0456\u0441\u0442\u0430\u0442\u0438\u0441\u044F \u0434\u043E \u0456\u043D\u0448\u043E\u0433\u043E \u043C\u0456\u0441\u0442\u0430";
+    }
+  } else {
+    el.previousElementSibling.innerText = "\u0412\u0432\u0435\u0434\u0438\u0442\u044C \u043A\u043E\u0440\u0440\u0435\u043A\u0442\u043D\u043E \u0432\u0456\u0434\u0441\u0442\u0430\u043D\u044C \u0432 \u043A\u0456\u043B\u043E\u043C\u0435\u0442\u0440\u0430\u0445 \u043C\u0456\u0436 \u0434\u0432\u043E\u043C\u0430 \u043C\u0456\u0441\u0442\u0430\u043C\u0438";
+  }
+}
+
+function convertor(el) {
+  var curs = 37;
+  var number = +prompt('Введить долари(тільки число)');
+
+  if (!isNaN(number) && number && Math.sign(number) !== -1) {
+    el.previousElementSibling.innerText = "".concat(number, "$ - ").concat(number * curs, "UAH");
+  } else {
+    el.previousElementSibling.innerText = "\u0412\u0432\u0435\u0434\u0438\u0442\u044C \u043A\u043E\u0440\u0440\u0435\u043A\u0442\u043D\u043E \u0434\u043E\u043B\u0430\u0440\u0438";
+  }
 }
--- dz-js-1/assets/js/main.js
+++ dz-js-1/assets/js/main.js
@@ -1,23 +1,66 @@
-function revert() {
-    const number = +prompt('Enter number');
-    let result = 0;
-    //12345 => 51234;
-    if (number >= 10000 && number <= 99999) {
-        result = ((number % 10) * 10000) + Math.trunc(number/10);
-        alert(result);
-    } else {
-        alert('Please enter number between 10000 and 99999');
+//valid variable
+let name = 'Elena';
+let userName = 'Elena';
+let firsName = 'Elena'; //camelCase
+let current_Name = 'Elena'; //snack_case
+// invalid variable
+/*let n/* = 'Elena';
+let n123 = 'Elena';
+let 0terw = 'Elena';*/
+function hello() {
+    let user = prompt('Введить ваше ім’я');
+    if (user) {
+document.getElementById('hello').innerText = `Привіт, ${user}!`
     }
-}
-function kl2ml() {
-    const number = +prompt('Enter km');
-    const k = 0.621371;
-    let result = 0;
-    if (number) {
-        result = number * k;
-        alert(`in ${number}km - ${result}ml` );
+};
+hello();
+
+function age(el) {
+   let userAge = +prompt('Введить ваше рік народження');
+   const date = new Date;
+   const currentYear = date.getFullYear();
+   let resultAges = currentYear - userAge;
+   if (!isNaN(userAge) && userAge && resultAges > 0 && resultAges < 120) {
+      el.previousElementSibling.innerText = `Ваш вік - ${resultAges}`;
     } else {
-        alert('Please enter number');
-    }
+        el.previousElementSibling.innerText = `Введить корректно ваш рік народження`;
+    };
+};
+function perimetr (el) {
+   let number = +prompt('Введить довжину сторони квадратa');
+   if (!isNaN(number) && number && (Math.sign(number) !== -1)) {
+      el.previousElementSibling.innerText = `периметр квадратa - ${number * 4}`;
+   } else {
+      el.previousElementSibling.innerText = `Введить корректно довжину сторони квадратa`;
+   }
 }
-if ("0") { alert("Hello World"); }
+function ploshaKola(el) {
+   let number = +prompt('Введить радіус кола');
+   if (!isNaN(number) && number && (Math.sign(number) !== -1)) {
+      el.previousElementSibling.innerText = `площa окружності - ${Math.PI * Math.pow(number, 2)}`;
+   } else {
+      el.previousElementSibling.innerText = `Введить корректно радіус кола`;
+   }
+}
+function speedUser(el) {
+   let lengthBetweenSities = +prompt('Введить відстань в кілометрах між двома містами(тільки число)');
+   if (!isNaN(lengthBetweenSities) && lengthBetweenSities && (Math.sign(lengthBetweenSities) !== -1)) {
+      let timeDirections =  +prompt('Введить за скільки годин ви хочете дістатися до іншого міста(тільки число)');
+      if (!isNaN(timeDirections) && timeDirections && (Math.sign(timeDirections) !== -1)) {
+         el.previousElementSibling.innerText = `Вам необхідно рухатись зі швидкістю ${lengthBetweenSities / timeDirections} км/год.`;
+      } else {
+         el.previousElementSibling.innerText = `Введить корректно за скільки годин ви хочете дістатися до іншого міста`;
+      }
+   } else {
+      el.previousElementSibling.innerText = `Введить корректно відстань в кілометрах між двома містами`;
+   }
+}
+ function convertor(el) {
+   const curs = 37;
+   let number = +prompt('Введить долари(тільки число)');
+   if (!isNaN(number) && number && (Math.sign(number) !== -1)) {
+      el.previousElementSibling.innerText = `${number}$ - ${number * curs}UAH`;
+   } else {
+      el.previousElementSibling.innerText = `Введить корректно долари`;
+   }
+ }
